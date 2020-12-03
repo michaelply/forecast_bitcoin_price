b82f3b52a0977666555e9fe5c1ae0cdc4e3caf13
Michael Poon <michaelpoon@Michaels-MacBook-Pro.local>
2020-12-03 15:19:01 -0800

    Added R script to generate our dataset

---
+++ bitcoin_data.R
@@ -0,0 +1,15 @@
+library(readr)
+##################Combine all data into one dataset####################
+bitcoin <- read_csv("bitcoinity_data.csv")
+str(bitcoin)
+google <- read_csv("multiTimeline.csv", skip=2)
+str(google)
+
+######
+dataset <- data.frame(cbind(bitcoin[-1,],google))
+names(dataset) <- c("bitcoin_time","bitcoin_price","bitcoin_volume","google_time","google_trend")
+dataset$week <- 1:260
+head(dataset)
+str(dataset)
+######
+write_csv(dataset,path = "bitcoin_data")
